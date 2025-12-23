aeb8d07623050a509b7ed6bb5b04ed63e5fc3260
Dw <70559591+WeiDu-CSB@users.noreply.github.com>
2025-12-23 15:07:05 +0800

    Add files via upload

--- BRD/BRD_main.m
+++ BRD/BRD_main.m
@@ -1,29 +1,40 @@
-
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % import BRD time course data reconstructed from Riching et al. fit 
 BRD_tc=importdata('BRD_tc.csv');
 BRD_data=BRD_tc.data(2:43,:);
 BRD_data_time=BRD_tc.data(1,:);
 BRD_data_anno=BRD_tc.textdata(2:43,:);
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % input parameters
+
+% constants
+V=4*10^(-12);   % cell volumn; L
+NA_MOLE=6.02*10^23;  % Avocado's number
+
+% target protein and E3 copy number; typical range: 1000 - 100000
 N_CRBN=12265;   % CRBN protein copy number
 N_VHL=15289;    % VHL protein copy number
 N_BRD2=12162;   % BRD2 protein copy number
 N_BRD3=37040;   % BRD3 protein copy number
 N_BRD4=27613;   % BRD4 protein copy number
 
+% target half-life; typical range: 0.5-200 hr
 Thalf_BRD2=20.8;   % BRD2 protein half-life; hr
 Thalf_BRD3=52.2;   % BRD3 protein half-life; hr
 Thalf_BRD4=41;     % BRD4 protein half-life; hr
 
+% degrader-target KD; typical range: 0.001-1 uM
 KD_BRD2=0.228;  % dBET1/MZ1 BRD2 KD; uM
 KD_BRD3=0.115;  % dBET1/MZ1 BRD3 KD; uM
 KD_BRD4=0.120;  % dBET1/MZ1 BRD4 KD; uM
 
+% degrader-E3 KD; typical range: 0.001-10 uM 
 KD_CRBN=1.795;  % dBET1 CRBN KD; uM
 KD_VHL=0.347;   % MZ1 VHL KD; uM
 
+% cooperativity; typical range: 0.1-100
 alpha_dBET1_BRD2=0.2;  % dBET1 BRD2 alpha; 
 alpha_dBET1_BRD3=0.2;  % dBET1 BRD3 alpha; 
 alpha_dBET1_BRD4=0.2;  % dBET1 BRD4 alpha; 
@@ -31,16 +42,17 @@ alpha_MZ1_BRD2=2.9;    % MZ1 BRD2 alpha;
 alpha_MZ1_BRD3=11;     % MZ1 BRD3 alpha;
 alpha_MZ1_BRD4=18;     % MZ1 BRD4 alpha;
 
+% degrader competitive displacement IC50 for intracellular concentration
+% calculation; typcial range: 0.01-1 uM
 IC50_JQ1=0.054;     % JQ1 competitive displacement IC50; uM
 IC50_dBET1=0.747;   % dBET1 competitive displacement IC50; uM
 IC50_MZ1=0.432;     % MZ1 competitive displacement IC50; uM
 
+% timespan of degradation; typical range: 30-2160 min
 T=5*60;              % timespan of degradation; min
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-V=4*10^(-12);   % cell volumn; L
-NA_MOLE=6.02*10^23;  % Avocado's number
-
-
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % parameter conversion
 T_CRBN=N_CRBN/V/NA_MOLE*10^6;   % CRBN protein concentration; uM
 T_VHL=N_VHL/V/NA_MOLE*10^6;     % VHL protein concentration; uM
@@ -55,16 +67,16 @@ kdp_BRD4=log(2)/Thalf_BRD4/60;   % BRD4 endogeneous protein degradation rate; mi
 ksp_BRD2=kdp_BRD2*T_BRD2;   % BRD2 endogeneous protein synthesis rate; uM/min;      
 ksp_BRD3=kdp_BRD3*T_BRD3;   % BRD3 endogeneous protein synthesis rate; uM/min; 
 ksp_BRD4=kdp_BRD4*T_BRD4;   % BRD4 endogeneous protein synthesis rate; uM/min; 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-
-
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % intracellular dose calculation
 dose_ori=[0.001 0.004 0.012 0.037 0.111 0.333 1];  % extracellular administrated concentration; uM
 dose_dBET1=10.^(log10(IC50_JQ1*1000)/log10(IC50_dBET1*1000)*log10(dose_ori*1000))/1000;  % dBET1 intracellular concentration; uM
 dose_dJQ1=10.^(log10(IC50_JQ1*1000)/log10(IC50_MZ1*1000)*log10(dose_ori*1000))/1000;  % JQ1 intracellular concentration; uM
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-
-
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % degradation rate fitting
 T_P=[T_BRD2 T_BRD3 T_BRD4];
 T_E3=[T_CRBN T_VHL];
@@ -75,15 +87,15 @@ k_dp=[kdp_BRD2 kdp_BRD3 kdp_BRD4];
 k_sp=[ksp_BRD2 ksp_BRD3 ksp_BRD4];
 dose=[dose_dBET1; dose_dJQ1];
 
+DMAX_data=reshape(BRD_data(:,501),7,6)';   % initialize DMAX data matrix
 
-DMAX_data=reshape(BRD_data(:,501),7,6)';
+kpr_series=10.^[-2:0.01:2];                % define kpr series 
+residual=zeros(6,length(kpr_series));      % initialize residual
 
-options=odeset('events',@events_time);
+options=odeset('events',@events_time);    % set ODE timepoints
 tic;
 
-kpr_series=10.^[-2:0.01:2];
-residual=zeros(6,length(kpr_series));
-
+% simulate degradation time course at various kpr values
 for k=1:length(kpr_series)
 
     disp(k)
@@ -110,12 +122,12 @@ for k=1:length(kpr_series)
 
 end
 
-[min_value min_index]=min(residual');
+[min_value min_index]=min(residual');     % select best fit kpr value
+k_pr=kpr_series(min_index);     
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-k_pr=kpr_series(min_index);
 
-
-
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % plot time course (Figure 2A)
 
 linecolor_1=bone(8);
@@ -201,7 +213,6 @@ set(gcf,'Units','inches');
 screenposition = get(gcf,'Position');
 set(gcf,'PaperPosition',[0 0 screenposition(3:4)],'PaperSize',[screenposition(3:4)]);
 saveas(gcf,'Figure2A.pdf');
-
 
 
 % plot DMAX (Figure 2B)
